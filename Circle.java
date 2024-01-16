0fadaf84c73c060087ddabfc1881010c9629dc62
Ulric <ulricaird1@gmail.com>
2024-01-16 19:15:24 -0400

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,20 +1,20 @@
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Circle {
 
-    static double pi = 3.14159265; // PI Variable
+    static final double pi = 3.14159265; // PI Variable
 
     // This handles the inputs for the radius
     public static void main(String[] args) {
 
-        try ( Scanner scanner = new Scanner(System.in) ) { // Creates a safe space for the Scanner
-            
+        try ( Scanner s = new Scanner(System.in) ) { // Creates a safe space for the Scanner
             System.out.print("Enter the Radius of the Circle: ");
-            int radius = scanner.nextInt(); // User Inputs Radius
-            double Area =  ((radius * radius) * pi); // Area Calculation Function
+            double radius = s.nextDouble();
 
-            System.out.println("Area: " + Area);
+            double Area =  ((radius * radius) * pi); // Area Calculation Function
+            System.out.println("Area: " + Area); // Displaying the output
         }
+
     }
 }
