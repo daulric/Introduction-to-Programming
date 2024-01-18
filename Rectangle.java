ca58d0aa2f2fba70f7caf0705e1909fa603c4c58
Ulric <ulricaird1@gmail.com>
2024-01-18 13:17:30 -0400

    Update Rectangle.java
    Just some changes

--- Rectangle.java
+++ Rectangle.java
@@ -5,14 +5,14 @@ public class Rectangle {
     public static void main(String[] args) {
         
         // Stores the value for Rectangle 1
-        int width1;
-        int length1;
-        int Rectangle_Area_1;
+        double width1;
+        double length1;
+        double Rectangle_Area_1;
 
         // Stores the value for Rectangle 2
-        int width2;
-        int length2;
-        int Rectangle_Area_2;
+        double width2;
+        double length2;
+        double Rectangle_Area_2;
 
 
         try ( Scanner s = new Scanner(System.in) ) {
@@ -20,9 +20,9 @@ public class Rectangle {
             System.out.println("============\nRectangle 1\n============\n");
 
             System.out.print("Input Width for Rectangle 1: ");
-            width1 = s.nextInt();
+            width1 = s.nextDouble();
             System.out.print("Input Length for Rectangle 1: ");
-            length1 = s.nextInt();
+            length1 = s.nextDouble();
 
             Rectangle_Area_1 = (length1 * width1);
             System.out.println("Rectangle 1 Area: " + Rectangle_Area_1);
@@ -31,9 +31,9 @@ public class Rectangle {
             System.out.println("\n============\nRectangle 2\n============\n");
 
             System.out.print("Input Width for Rectangle 2: ");
-            width2 = s.nextInt();
+            width2 = s.nextDouble();
             System.out.print("Input Length for Rectangle 2: ");
-            length2 = s.nextInt();
+            length2 = s.nextDouble();
 
             Rectangle_Area_2 = (length2 * width2);
             System.out.println("Rectangle 2 Area: " + Rectangle_Area_2);
@@ -44,9 +44,9 @@ public class Rectangle {
 
         if (Rectangle_Area_1 == Rectangle_Area_2) {
             System.out.println("Both Rectangle Areas are the Same!");
-        } else if (Rectangle_Area_1 >= Rectangle_Area_2) {
+        } else if (Rectangle_Area_1 > Rectangle_Area_2) {
             System.out.println("Rectangle 1 Area is greater than Rectangle Area 2");
-        } else if (Rectangle_Area_1 <= Rectangle_Area_2) {
+        } else if (Rectangle_Area_1 < Rectangle_Area_2) {
             System.out.println("Rectangle 2 Area is greater than Rectangle Area 1");
         }
 
