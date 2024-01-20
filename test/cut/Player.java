fa46f04c8d48869efcf5b501d5f64fe579b372b1
Ulric <ulricaird1@gmail.com>
2024-01-19 20:09:11 -0400

    Experimenting With Java Imports

---
+++ test/cut/Player.java
@@ -0,0 +1,11 @@
+package test.cut;
+
+public class Player {
+    public static String Name() {
+        return "daulric";
+    }
+
+    public static double Id() {
+        return Math.random();
+    }
+}
---
+++ test/cut/main_new.java
@@ -0,0 +1,17 @@
+// Testing Import with Java
+
+package test.cut;
+
+public class main_new {
+    public static void main(String[] args) {
+        String Name = Player.Name();
+        double Id = Player.Id();
+        
+        System.out.print(
+            "Player Name: " + Name + 
+            "\n" + 
+            "ID: " + Id
+        );
+
+    }
+}
