9a6415dab6c263194dcb5bc6c9bd236969862583
Ulric <ulricaird1@gmail.com>
2024-01-21 12:25:05 -0400

    Create Payroll.cpp
    This is for my input validation assessment

---
+++ C++/Payroll.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace  std;
+
+bool checkPayRange(double input) {
+    if (input >= 7.50 && input <= 18.25 )  {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+bool checkHourlyRange(double input) {
+    if (input >= 0 && input <= 40) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    double hourly_worked;
+    double pay_rate;
+
+    cout << "Enter Pay Rate: ";
+    cin >> pay_rate;
+
+    cout << "Enter Hours Worked: ";
+    cin >> hourly_worked;
+
+    bool check_pay = checkPayRange(pay_rate);
+    bool check_hourly = checkHourlyRange(hourly_worked);
+
+    if (!check_pay) {
+        cerr << "The Pay Range supposed to be between $7.50 and $18.25" << endl;
+        return 1;
+    }
+
+    if (!check_hourly) {
+        cerr << "The Hourly Range suppose to be between 0 - 40 hours" << endl;
+        return 1;
+    }
+
+    double gross_pay = (hourly_worked * pay_rate);
+    cout << "Gross Pay: $" << gross_pay;
+
+    return 0;
+}
