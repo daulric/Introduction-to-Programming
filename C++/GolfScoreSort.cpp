5ce1ac92310352fb2c97fb78d14d96a70ddc746f
Ulric <ulricaird1@gmail.com>
2024-03-17 12:36:30 -0400

    Update GolfScoreSort.cpp
    Preparation for Submission

--- C++/GolfScoreSort.cpp
+++ C++/GolfScoreSort.cpp
@@ -9,14 +9,16 @@ int main() {
     int scores[10];
     int temp;
 
+    int sizeArray = (sizeof(scores) / sizeof(*scores)); // This is how you get the length of the array
+
     // Enter the Scores!
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < sizeArray; i++) {
         cout << "Enter Score " << i+1 << ": ";
         cin >> scores[i];
     }
 
     // Sorting the Array in ascending order
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < sizeArray; i++) {
         for (int j = i+1; j < 10; j++) {
             if (scores[i] > scores[j]) {
                 swap(scores[i], scores[j]); // Swapped scores[i] with sccores[j]
@@ -24,12 +26,14 @@ int main() {
         }
     }
 
-    cout << "============================" << endl;
+    cout << "=========== Ascending Order ============" << endl;
 
     // Displaying Sorted Outputs
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < sizeArray; i++) {
         cout << i+1 << ": " << scores[i] << endl;
     }
+
+    cout << "========================================" << endl;
 
     return 0;
 }
